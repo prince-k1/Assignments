2dcdb4bb941440eda2226b733ff84253d4bcbb3b
Prince Kumar Pandit <pp611223@gmail.com>
2025-05-05 17:46:40 +0530

    routes

--- Middleware/index.js
+++ Middleware/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
-let port = 3200;
+let port = 3000;
 
-app.use((req, res, next) => {
-    req.user = "Guest";
-    next();
+
+
+app.get('/orders', (req, res) => {
+    res.send('Here is the list of all orders');
 })
-
-app.get('/welcome', (req, res, next) => {
-    res.send(`Welcome, Guest!`);
+app.post('/orders', (req, res) => {
+    res.send('A new order has been created');
+})
+app.get('/users', (req, res) => {
+    res.send('Here is the list of all users');
+})
+app.post('/users', (req, res) => {
+    res.send('A new user has been created');
 })
 
 app.listen(port, () => {
