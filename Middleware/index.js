46ad17c5bd3ab7ec36ca28f4f708f0ae71978004
Prince Kumar Pandit <pp611223@gmail.com>
2025-05-05 18:47:29 +0530

    Router and Filters

--- Middleware/index.js
+++ Middleware/index.js
@@ -2,27 +2,13 @@ const express = require('express');
 const app = express();
 let port = 4000;
 
-app.use((req, res, next) => {
-    console.log(`${req.method} made to ${req.path}`);
-    next();
-})
+const categoriesRouter = require('./routes/categories');
+const productsRouter = require('./routes/products');
 
-app.get('/products', (req, res) => {
-    res.send('Here is the list of all products');
-})
-app.post('/products', (req, res) => {
-    res.send('A new product has been created');
-})
-app.get('/categories', (req, res) => {
-    res.send('Here is the list of all categories');
-})
-app.post('/categories', (req, res) => {
-    res.send('A new catagory has been created');
-})
 
-app.use('/', (req, res) => {
-    res.end('<h1>404 Page is not found</h1>')
-})
+
+app.use('/categories', categoriesRouter);
+app.use('/products', productsRouter);
 
 app.listen(port, () => {
     console.log('server is running');
