c1ad05de8c32d1e16d122c131a2259196feca7ba
Prince Kumar Pandit <pp611223@gmail.com>
2025-05-05 18:53:05 +0530

    Router and Filters - Task

--- Middleware/index.js
+++ Middleware/index.js
@@ -4,11 +4,12 @@ let port = 4000;
 
 const categoriesRouter = require('./routes/categories');
 const productsRouter = require('./routes/products');
-
+const booksRouter = require('./routes/books');
 
 
 app.use('/categories', categoriesRouter);
 app.use('/products', productsRouter);
+app.use('/books', booksRouter);
 
 app.listen(port, () => {
     console.log('server is running');
