fb569f4ace605af2fa9adbb20a3bfb45ef82910c
Prince Kumar Pandit <pp611223@gmail.com>
2025-08-27 17:18:52 +0530

    Create Your Own Middleware and Routing

--- 01Express/index.js
+++ 01Express/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.use('/welcome', (req, res) => {
+    res.send('<h1>Welcome! Guest</h1>')
+})
 app.listen(3000, () => {
     console.log('server is running');
 })
