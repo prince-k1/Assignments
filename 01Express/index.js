5cd76dfce3b02137c8288cf43b01b6026802fdd6
Prince Kumar Pandit <pp611223@gmail.com>
2025-08-27 22:20:52 +0530

    Router and Filters - Task

--- 01Express/index.js
+++ 01Express/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const productRouter = require('./routes/product');
 const categoriesRouter = require('./routes/categories');
-
+const bookRouter = require('./routes/books');
 
 
 app.use('/product', productRouter);
 
 app.use('/categories', categoriesRouter);
+app.use('/books', bookRouter);
 
 // Start the server
 app.listen(4000, () => {
