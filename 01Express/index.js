ee95bdeebf59ca8f16fcbed5120d6d9aa64bcfaf
Prince Kumar Pandit <pp611223@gmail.com>
2025-08-27 22:17:15 +0530

    Router and Filters

--- 01Express/index.js
+++ 01Express/index.js
@@ -1,31 +1,13 @@
 const express = require('express');
 const app = express();
+const productRouter = require('./routes/product');
+const categoriesRouter = require('./routes/categories');
 
-// Middleware to log HTTP method and endpoint
-app.use((req, res, next) => {
-    console.log(`${req.method} request made to ${req.url}`);
-    next();
-});
 
-// GET /products
-app.get('/products', (req, res) => {
-    res.send('Here is the list of all products.');
-});
 
-// POST /products
-app.post('/products', (req, res) => {
-    res.send('A new product has been added.');
-});
+app.use('/product', productRouter);
 
-// GET /categories
-app.get('/categories', (req, res) => {
-    res.send('Here is the list of all categories.');
-});
-
-// POST /categories
-app.post('/categories', (req, res) => {
-    res.send('A new category has been created.');
-});
+app.use('/categories', categoriesRouter);
 
 // Start the server
 app.listen(4000, () => {
