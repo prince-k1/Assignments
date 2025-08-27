a00fbbde52611d30b59f25ae5734e5f5cf1b76bf
Prince Kumar Pandit <pp611223@gmail.com>
2025-08-27 17:21:12 +0530

    Question-Routes

--- 01Express/index.js
+++ 01Express/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
 
-app.use('/welcome', (req, res) => {
-    res.send('<h1>Welcome! Guest</h1>')
-})
+// Middleware to parse JSON request bodies (optional for POST)
+app.use(express.json());
+
+// GET /orders
+app.get('/orders', (req, res) => {
+    res.send('Here is the list of all orders.');
+});
+
+// POST /orders
+app.post('/orders', (req, res) => {
+    res.send('A new order has been created.');
+});
+
+// GET /users
+app.get('/users', (req, res) => {
+    res.send('Here is the list of all users.');
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+    res.send('A new user has been added.');
+});
+
+// Start the server on port 3000
 app.listen(3000, () => {
-    console.log('server is running');
-})
+    console.log('Server is running on port 3000');
+});
